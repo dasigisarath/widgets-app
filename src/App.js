4baf2eba9bf3a1f01075e38598cb13febe5ac579
unknown <dasigisarath@gmail.com>
2020-08-18 16:16:49 +0530

    widgets-app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
-function App() {
+const items = [
+  {
+    title: "what is a React ?",
+    content: "React is a front end js framework",
+  },
+  {
+    title: "what is java ?",
+    content: "java is the best oop language ever",
+  },
+  {
+    title: "what is Python ?",
+    content: "Python is a powerfull pograming language",
+  },
+];
+
+export default () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
   );
-}
-
-export default App;
+};
